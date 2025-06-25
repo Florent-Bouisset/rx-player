7d61871574d0ed9c5e373ca9cca4e7e1c8fdaacb
Florent Bouisset <florent.bouisset@gmail.com>
2025-06-25 14:34:34 +0200

    don't seek directly after loaded metaData

--- src/compat/can_seek_directly_after_loaded_metadata.ts
+++ src/compat/can_seek_directly_after_loaded_metadata.ts
@@ -1,9 +1,9 @@
-import { isSafariMobile } from "./browser_detection";
+import { isSafariDesktop, isSafariMobile } from "./browser_detection";
 
 /**
  * On safari mobile (version 17.1.2) seeking too early cause the video to never buffer
  * media data. Using delaying mechanisms such as `setTimeout(fn, 0)` defers the seek
  * to a moment at which safari should be more able to handle a seek.
  */
-const canSeekDirectlyAfterLoadedMetadata = !isSafariMobile;
+const canSeekDirectlyAfterLoadedMetadata = !isSafariMobile && !isSafariDesktop;
 export default canSeekDirectlyAfterLoadedMetadata;
